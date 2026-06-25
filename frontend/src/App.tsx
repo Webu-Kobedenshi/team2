88587920bb825f31dd38a737c641cfb7a3e6a0d4
Kyosuke Sato <kyokke.matsu@gmail.com>
2026-06-25 17:29:33 +0900

    Revert "Feature/sara 6 home"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
 import { Home } from "./pages/Home";
-import { HowToPlay } from "./pages/HowToPlay";
 
 function App() {
-  const [page, setPage] = useState("home");
-
-  return page === "home" ? (
-    <Home onShowHowToPlay={() => setPage("howtoplay")} />
-  ) : (
-    <HowToPlay onBack={() => setPage("home")} />
-  );
+  return <Home />;
 }
 
 export default App;
